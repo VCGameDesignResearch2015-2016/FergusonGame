0f2202b4c84e31935b6806701887161901f4c757
kellyyu5195 <keyu@vassar.edu>
2015-11-23 02:23:28 -0500

    working on news scene

---
+++ Ferguson VR/Assets/Scripts/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	public GameObject nyt;
+	public GameObject huff;
+	public GameObject bbc;
+	public GameObject cnn;
+	MeshCollider coll;
+	GameObject loc;
+
+	// Use this for initialization
+	void Start () {
+		coll = GetComponent<MeshCollider> ();
+		loc = GameObject.FindGameObjectWithTag ("surface");
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Player") {
+			Instantiate(nyt, loc.transform.position, Quaternion.identity);
+		}
+	}
+}
